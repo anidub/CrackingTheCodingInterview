4a33f87b30700c423ca971fdd89716a9eeb81ed4
Anish D <anish_dubey@hotmail.com>
2017-04-27 19:31:03 -0400

    desc

--- src/treesandgraph2/graphtopologicalsort.java
+++ src/treesandgraph2/graphtopologicalsort.java
@@ -4,16 +4,24 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class graphtopologicalsort {
-
-	public void topological(){
+//http://www.geeksforgeeks.org/topological-sorting/
+/*	Topological sorting for Directed Acyclic Graph (DAG) is a linear ordering of vertices such that 
+	for every directed edge uv, vertex u comes before v in the ordering. Topological Sorting for
+	a graph is not possible if the graph is not a DAG.
+	For example, a topological sorting of the following graph is “5 4 2 3 1 0”. There 
+	can be more than one topological sorting for a graph. For example, another topological sorting 
+	of the following graph is “4 5 2 3 1 0”. The first vertex in topological sorting is always
+	a vertex with in-degree as 0 (a vertex with no in-coming edges).	*/
+	public void topological(graph graph){
 		Stack stack = new Stack();
-		boolean[] visited = new boolean[V];
-		for(int i =0 ; i < V;i++){
+		int size = graph.adj.length;
+		boolean[] visited = new boolean[size];
+		for(int i =0 ; i < size;i++){
 			visited[i] = false;
 		}
-		for(int i = 0; i < V;i++){
+		for(int i = 0; i < size;i++){
 			if(visited[i] == false){
-				topoligicalsort(i,visited,stack);
+				topoligicalsort(i,graph, visited,stack);
 			}
 		}	
 		while(!stack.isEmpty()){
@@ -21,14 +29,14 @@ public class graphtopologicalsort {
 		}
 }
 	
-	public void topological(int v, boolean[] visited, Stack stack){
+	public void topoligicalsort(int v, graph graph, boolean[] visited, Stack stack){
 		int i;
 		visited[v] = true;
-		Iterator<Integer> it = adj[v].ListIterator();
+		Iterator<Integer> it = graph.adj[v].listIterator();
 		while(it.hasNext()){
 			i = it.next();
 			if(!visited[i]){
-				topological(i,visited,stack);
+				topoligicalsort(i,graph, visited,stack);
 			}
 		}
 		stack.push(v);
