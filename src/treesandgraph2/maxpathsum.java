d2a8522e98c38405c83d77a00f0ae54f451ae665
Anish D <anish_dubey@hotmail.com>
2017-04-29 20:27:13 -0400

    desc

--- src/treesandgraph2/maxpathsum.java
+++ src/treesandgraph2/maxpathsum.java
@@ -3,6 +3,15 @@ package treesandgraph2;
 public class maxpathsum {
 //Given a binary tree, find the maximum path sum. The path may start and end at any node in the tree.
 	//http://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
+/*	For each node there can be four ways that the max path goes through the node:
+		1. Node only
+		2. Max path through Left Child + Node
+		3. Max path through Right Child + Node
+		4. Max path through Left Child + Node + Max path through Right Child
+
+		The idea is to keep trace of four paths and pick up the max one in the end. 
+		An important thing to note is, 	root of every subtree need to return maximum path sum such that 
+		at most one child of root is involved. This is needed for parent function call.*/
 	//Time Complexity: O(n) 
 	public int maxPathSum(Node root) {
 		int max[] = new int[1]; 
@@ -29,7 +38,5 @@ public class maxpathsum {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
 }
