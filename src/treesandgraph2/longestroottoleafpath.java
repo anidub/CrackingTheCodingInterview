843a767898d46f9accf2ea4f5ea664572e0b87f9
Anish D <anish_dubey@hotmail.com>
2017-05-04 00:13:56 -0400

    new method

--- src/treesandgraph2/longestroottoleafpath.java
+++ src/treesandgraph2/longestroottoleafpath.java
@@ -1,19 +1,11 @@
 package treesandgraph2;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-
-//wrong code
-
 public class longestroottoleafpath {
-
-	static Node root;
-	static ArrayList<int[]> allpaths= new ArrayList<int[]>();
-	
+	static Node root;	
 	public static void main(String[] args) {
-		longestroottoleafpath tree = new longestroottoleafpath();
-		
+		longestroottoleafpath tree = new longestroottoleafpath();		
 		tree.root = new Node(10);
 		tree.root.left = new Node(8);
 		tree.root.right = new Node(2);
@@ -21,49 +13,40 @@ public class longestroottoleafpath {
 		tree.root.left.right = new Node(5);
 		tree.root.right.left = new Node(2);
 		tree.root.right.left.left = new Node(22);
-		tree.printPaths(root);
-			//for(int i = 0; i < allpaths.size(); i++){
-		//	for(int j = 0; j < allpaths.get(i).length; j++){
-		//		System.out.print(allpaths.get(i)[j]);
-		//	}
-		//	System.out.println();
-		//}
-		/*for(int i = 0; i < array.length; i++){
-			System.out.print(array[i] + " ");
-		}*/
-		
-	}
-
-	public void printPaths(Node root){
-		//int[] paths = new int[10];
-		ArrayList<Integer> paths = new ArrayList<>();
-		printPathsRecur(root, paths,0);
+		tree.longest(root);
 	}
 	
-	public void printPathsRecur(Node root, ArrayList<Integer> paths,int pathlen){
-		if(root == null) return;
-		int[] array = new int[10];
-		Queue<Node> q =  new LinkedList<Node>();
-		q.add(root);
-		while(!q.isEmpty()){
-			Node n = q.poll();
-			array[pathlen] = n.data;
-			pathlen++;
-			if(n.left == null && n.right == null){	
-				printArray(array,pathlen);
+	public static void longest(Node root){
+		Queue<Object> queue = new LinkedList<>();
+		queue.add(root);
+		queue.add(root.data+" ");
+		int maxlen = 0;
+		String longestpath = "";
+		while(!queue.isEmpty()){
+			Node node = (Node) queue.poll();
+			String headstring = (String) queue.poll();
+			
+			if(node.left == null && node.right == null){
+				//System.out.println(headstring);
+				if(maxlen < headstring.length()){
+					maxlen = headstring.length();
+					longestpath = headstring;
+				}
+				continue;
 			}
-			if(n.left != null) q.add(n.left);
-			if(n.right != null) q.add(n.right);
+			
+			if(node.left != null){
+				String leftstring = headstring + node.left.data + " ";
+				queue.add(node.left);
+				queue.add(leftstring);
+			}
+			
+			if(node.right != null){
+				String rightstring = headstring + node.right.data + " ";
+				queue.add(node.right);
+				queue.add(rightstring);
+			}
 		}
-	}
-		
-	public void printArray(int[] paths, int pathlen){
-		int i;
-		//allpaths.add(paths) ; 
-		for(i = 0; i < pathlen; i++){
-			System.out.print(paths[i] + " ");
-		}
-		System.out.println();
-		
+		System.out.println(longestpath);
 	}
 }
