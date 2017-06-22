72bc0d96fa6199f852c7d5c804d48231c4fc0bf8
anidub <anish_dubey@hotmail.com>
2017-06-22 16:54:20 -0400

    Update insertInSortedLinkedList.java

--- src/Linkedlist/insertInSortedLinkedList.java
+++ src/Linkedlist/insertInSortedLinkedList.java
@@ -8,34 +8,40 @@ public class insertInSortedLinkedList {
 		n.next.next = new Node(6);
 		n.next.next.next = new Node(8);
 		int[] arr = {5,3,7};
-		insert(n,arr);
+		display(insert(n,arr));
 	}
 	
-	public static void insert(Node n, int[] arr) {
-		Node current = n;
-		Node displayNode = current;
-		for (int i = 0; i < arr.length; i++) {
+	public static Node insert(Node head, int[] arr){
+		if(head == null || arr.length == 0) return head;
+		Node current = head;
+		Node display = new Node(-10);
+		display.next = current;
+		for(int i = 0; i < arr.length; i++){
 			int k = arr[i];
-			if (k < n.data) {
-				Node newN = new Node(k);
-				newN.next = n;
-			} else {
-				while (current != null) {
-					if (k < current.next.data) {
-						Node newN = new Node(k);
-						newN.next = current.next;
-						current.next = newN;
+			if(k < head.data){
+				Node newNode = new Node(k);
+				newNode.next = head;
+				head = newNode;
+			}else{
+				current = head;
+				while(current != null){
+					if(k < current.next.data){
+						Node newNode = new Node(k);
+						newNode.next = current.next;
+						current.next = newNode;
 						break;
-					} else {
+					}else{
 						current = current.next;
 					}
-
 				}
 			}
 		}
-		while (displayNode != null) {
-			System.out.print("->" + displayNode.data);
-			displayNode = displayNode.next;
-		}
+		return head;
+	}
+	public static void display(Node head){
+		while(head != null){
+			System.out.print(head.data + " ");
+			head = head.next;
+		}		
 	}
 }
